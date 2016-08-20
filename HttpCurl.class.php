941ced378bc7ac7ececee95aff25bc0dfb77c420
cy <cy@cydeMacBook-Pro.local>
2016-08-20 15:41:45 +0800

    HttpCurl

--- HttpCurl.class.php
+++ HttpCurl.class.php
@@ -1,16 +1,18 @@
 <?php
 class Lib_HttpCurl {
 
-	public function get($urli,$header=array()){
+	public function get($url,$header=array()){
 		//初始化
 		$ch = curl_init();
 		//设置选项,包括URL
 		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		if(empty($header))
-			curl_setopt($ch, CURLOPT_HEADER, 0);
-		else
-			curl_setopt($ch, CURLOPT_HEADER, $header);
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+        else{
+                curl_setopt($ch,CURLOPT_HEADER,0);
+                curl_setopt($ch,CURLOPT_HTTPHEADER,$header);
+        }
 		//执行并获取html文档内容
 		$output = curl_exec($ch);
 		//释放curl句柄
